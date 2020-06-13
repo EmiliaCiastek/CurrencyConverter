337e0aa8d682fa92910ef76ea411265de06e4da0
Emilia Ciastek <emilia.ciastek@farmaprom.pl>
2020-06-13 11:40:26 +0200

    Change layout

--- app/src/main/java/com/ciastek/currencyconverter/view/CurrencyViewHolder.kt
+++ app/src/main/java/com/ciastek/currencyconverter/view/CurrencyViewHolder.kt
@@ -10,6 +10,7 @@ class CurrencyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         itemView.currency_shortcut.text = currency.shortcut
         itemView.currency_name.text = currency.fullName
         itemView.currency_value.setText(currency.value.toString())
+        itemView.currency_value.isEnabled = false
         itemView.country_flag_thumbnail.setImageDrawable(itemView.context.getDrawable(currency.flagThumbnail))
     }
 }
